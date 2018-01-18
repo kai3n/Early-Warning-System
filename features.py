88465a23bf6282540e7f7e94a40a6c56684165b2
Mingun Pak <mingunpa@usc.edu>
2018-01-18 15:40:04 -0800

    Added words list

---
+++ features.py
@@ -0,0 +1,5 @@
+NEGATIVE_WORDS_LIST = ['drunk', 'out of control', 'found drunk', 'shirtless', 'spit', 'paparazzo', 'found guilty',
+                       'guilty', 'assault', 'careless', 'reckless', 'probation', 'punching', 'spitting', 'throwing',
+                       'egg', 'pissing', 'recklessly', 'accident', 'divorce', 'sex', 'break up', 'scandal', 'dead', 'die',
+                       'kill', 'gun', 'sexual', 'sexually', 'sexually assault', 'victim', 'police']
+POSITIVE_WORDS_LIST = ['happy', 'celebrating']
